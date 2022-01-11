cfd979d43ed3de29ebf3f3324f4bd5d5d59b2dea
lhervi <lhervi@gmail.com>
2022-01-11 11:03:04 -0400

    update 202201111104

--- testfecha.php
+++ testfecha.php
@@ -2,7 +2,15 @@
 <html>
 <body onload="maxYmin()">
 
-<input type="month" id="myMonth" min="2014-04" max="" >
+<?php
+include_once './controller/utils/classFechas.php';
+$fecMax = date("Y-m");
+$monthMin = intval(date('m'))-7;
+$añoMin = $monthMin>=0 ? date("Y") : strval(intval(date("Y"))-1);
+$fecMin= $añoMin . $monthMin;
+echo '<input type="month" id="myMonth" min="' . $fecMin . '" max="' . $fecMax .  '" >';
+
+?>
 
 <p>Click the button to display the value of the max attribute of the month field.</p>
 
@@ -17,45 +25,56 @@
 
 <script>
 
-/*
-const videoElement = document.querySelector('video');
-videoElement.audioTracks.onchange = (event) => {
-    console.log(`'${event.type}' event fired`);
-};
-*/
-
 document.getElementById("myMonth").addEventListener("change", (e) => {
   
-  const month = document.getElementById("myMonth");  
-    //convertir month.value en un valor fecha comparable con month.max
-    //Esto implica crear una función que dada año-mes retorne un número
-    if(month.value>month.max || month.value<month.min){
+  const month = document.getElementById("myMonth");      
+
+    if(comparaFecha(month.value, month.max) || comparaFecha(month.min, month.value)){
       alert("el mes no puede ser mayor al mes actual ni menor a " +  month.min);   
+      document.getElementById("myMonth").value="";
     }
+    
+    
   });
 
-function maxYmin(){
-  const d= new Date();
-  const fec = d.getFullYear() + "-" + (d.getMonth()+1);
-  document.getElementById("myMonth").max = fec; 
-  document.getElementById("myMonth").min = minFec(); 
-}
+  function comparaFecha(fec1, fec2){
+    fecInt1=fechaInt(fec1);
+    fecInt2=fechaInt(fec2);
+    if((fecInt1-fecInt2)>=0){
+      return true
+    }else{
+      return false
+    }
+  }
 
-function myFunction() {
-  const max = document.getElementById("myMonth").max;
-  const min = document.getElementById("myMonth").min;
-  document.getElementById("demo1").innerHTML = "el mínimo es: " + min;
-  document.getElementById("demo2").innerHTML = "el máximo es: " + max;
-}
+  function fechaInt(fec){  
+  //regresa un int que representa la fecha
+  fecInt = Date.parse(fec = fec + "-01"); 
+  return fecInt;
+  }
 
-function minFec(){
-    //fecha de hoy menos 7 meses, considerando la posiblidad de 6 meses atrás sin incluir el que está corriendo
+  function maxYmin(){
     const d= new Date();
-    const mesActual = d.getMonth(); //regresa el mes en formato numérico del 0 al 11    
-    const año = (mesActual<7) ? d.getFullYear()-1 : d.getFullYear();    
-    const mesMin = año + "-" + ((Math.abs(d.getMonth()-7))+1);
-    return mesMin;
-}
+    const fec = d.getFullYear() + "-" + (d.getMonth()+1);
+    document.getElementById("myMonth").max = fec; 
+    document.getElementById("myMonth").min = minFec(); 
+  }
+
+  function myFunction() {
+    const max = document.getElementById("myMonth").max;
+    const min = document.getElementById("myMonth").min;
+    document.getElementById("demo1").innerHTML = "el mínimo es: " + min;
+    document.getElementById("demo2").innerHTML = "el máximo es: " + max;
+  }
+
+  function minFec(){
+      //fecha de hoy menos 7 meses, considerando la posiblidad de 6 meses atrás sin incluir el que está corriendo
+      const d= new Date();
+      const mesActual = d.getMonth(); //regresa el mes en formato numérico del 0 al 11    
+      const año = (mesActual<7) ? d.getFullYear()-1 : d.getFullYear();    
+      const mesMin = año + "-" + ((Math.abs(d.getMonth()-7))+1);
+      return mesMin;
+  }
 
 </script>
 
