5c4bdccf55f24cfdba54ebb68ed6f661403a1d49
lhervi <lhervi@gmail.com>
2022-03-09 12:49:57 -0400

    update 202203091253

---
+++ controller/utils/classLookAndFeel.php
@@ -0,0 +1,41 @@
+<?php
+
+class LookAndFeel{
+    
+    /**
+     * enlace método estático que asigna un enlace a un id dado a través de una función javascript que se activa 
+     * al hacer click sobre el elemento
+     *
+     * @param  string $id el id del elemento
+     * @param  string $link el URL al que va a apuntar el link que se asigna al elemento click
+     * @return string regresa una cadena con la función javascript que apunta al enlace  
+     */
+    static function enlace(string $id, string $link){
+        $func = "document.getElementById('$id').addEventListener('click',function(){";    
+        $func .= PHP_EOL . "location.href ='" . $link . "';" . PHP_EOL . "})" . PHP_EOL;                    
+        return $func;
+    }
+        
+    /**
+     * clase método estático que regresa un <div> que contiene un <spam> y cuyo
+     *
+     * @param  string $str un string con el mensaje a insertar
+     * @param  int $tam Determina el tamaño de la fuente y por defecto su valor es 3
+     * @return string Regresa un string de la siguiente forma: <div class=[estatus1|estatus2]><spam>El contenido</spam></div>
+     */
+    static function estatusX($str, int $tam=3){
+            static $val=true;
+            
+            if ($val){
+                $val = false;
+                return "<div class='estatus1'><spam><h" . $tam . ">" . $str . "</h" . $tam . "<spam></div>";            
+            }else{
+                $val = true;
+                return "<div class='estatus2'><spam><h" . $tam . ">" . $str . "</h" . $tam . "<spam></div>";           
+            }
+        }
+
+}
+
+
+?>
--- test12.php
+++ test12.php
@@ -2,71 +2,24 @@
  
  include_once './constantes.php';
  include HOME . '/view/encabezado.php';
-
- function enlace(string $id, string $link){
-    $func = "document.getElementById('$id').addEventListener('click',function(){";    
-    $func .= PHP_EOL . "location.href ='" . $link . "';" . PHP_EOL . "})" . PHP_EOL;                    
-    return $func;
- }
-
-    $clase = function($str, int $tam=3){
-        static $val=true;
-        
-        if ($val){
-            $val = false;
-            return "<div class='estatus1'><h" . $tam . ">" . $str . "</h" . $tam . "</div>";            
-        }else{
-            $val = true;
-            return "<div class='estatus2'><h" . $tam . ">" . $str . "</h" . $tam . "</div>";           
-        }
-    }
-
-    
+ include_once HOME . '/controller/utils/classLookAndFeel.php'; 
 
 ?>
 
-
 <body>
 <br/>
+
 <?php
 
-function estiloEstatus(){
-    return null;
-}
-
- echo $clase(" Culminó con éxito la carga de los registros");
- echo $clase("resultado1");      
- echo $clase("prueba de resultados");
- echo $clase("mas resultados que mostrar");
- echo '<div id="regresar" style="cursor:pointer; width:200px"><h3>>>>Regresar</h3></div>';
+ echo LookAndFeel::estatusX(" Culminó con éxito la carga de los registros");
+ echo LookAndFeel::estatusX("resultado1");      
+ echo LookAndFeel::estatusX("prueba de resultados");
+ echo LookAndFeel::estatusX("mas resultados que mostrar");
+ echo '<div id="regresar" class="estatus" style="cursor:pointer; width:200px"><h4>>>>Regresar</h4></div>';
  echo "<script>" . PHP_EOL;      
- echo (enlace('regresar', INICIO));
+ echo (LookAndFeel::enlace('regresar', INICIO));
  echo "</script>";
-
-
-// scriptTag(enlace('regresar', INICIO));
-
-/*
-
-function scriptTag($func, $param){
-    
-    echo "<script>" . PHP_EOL;
-    
-    if(is_array($param)){
-        $func(implode(" ,", $param));
-    }else{
-        $func($param);
-    }
-    
-    echo "</script>";
- }
-
-$func = function (string $fName, array $param){     
-    return [$fName](explode(" ,", $param));
-}
-
-$arg = array('regresar', INICIO);
-*/
+ 
 ?>
 
 </body>
